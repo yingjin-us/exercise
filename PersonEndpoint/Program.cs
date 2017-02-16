7ee9a0a31743b5232d1e60899b6c974700855c23
weasterner <yingjin.us@gmail.com>
2017-02-16 10:17:11 -0500

    Create an endpoint returning hardcoded data

---
+++ PersonEndpoint/Program.cs
@@ -0,0 +1,46 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEndpoint
+{
+    class Program
+    {
+        static void Main()
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            Console.Title = "Person Endpoint";
+
+            var endpointConfiguration = new EndpointConfiguration("Person");
+
+            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.EnableInstallers();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("2 Person objects with these fields and anything for the values:");
+            Console.WriteLine("\tFirstName");
+            Console.WriteLine("\tLastName");
+            Console.WriteLine("\tJobTitle");
+
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
